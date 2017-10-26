6f10ab908844419708f6cf03ddf0d84e865be9aa
halvors <halvors@halvors.org>
2017-10-26 19:38:28 +0200

    Update.

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
 set minute=%time:~-8,2%
 set second=%time:~-5,2%
 
-java -jar launcher-builder-1.0.4-all.jar --version "%year%%month%%day%%hour%%minute%%second%" --input . --output upload --manifest-dest "upload/voltzplay.json" 
+java -jar launcher-builder-1.0.7-all.jar --version "%year%%month%%day%%hour%%minute%%second%" --input . --output upload --manifest-dest "upload/voltzplay.json" 
 
 PAUSE
