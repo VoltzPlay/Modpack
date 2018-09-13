05f410002887b4dc31d70a2babb6e254f0c7f5ce
Halvor Lyche Strandvoll <halvors@halvors.org>
2018-09-14 01:59:09 +0200

    Added new mods.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar launcher-builder-1.0.7-all.jar --version "$(date +"%Y%m%d%H%M%S")" --input . --output upload --manifest-dest "upload/voltzplay_1.12.2.json"
+java -jar launcher-builder-1.0.7-all.jar --version "$(date +"%Y%m%d%H%M%S")" --input . --output upload --manifest-dest "upload/cosmic_inflation_1.12.2.json"
