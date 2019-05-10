22ebfa3c4f6a383138bb92808e7afcc0254947ef
Halvor Lyche Strandvoll <halvors@halvors.org>
2019-05-10 16:32:35 +0200

    Update mod dependencies.

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar launcher-builder-1.0.7-all.jar --version "$(date +"%Y%m%d%H%M%S")" --input . --output upload --manifest-dest "upload/cosmic_inflation_1.12.2.json"
+#java -jar launcher-builder-1.0.7-all.jar --version "$(date +"%Y%m%d%H%M%S")" --input . --output upload --manifest-dest "upload/cosmic_inflation_1.12.2.json"
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar launcher-builder-1.0.7-all.jar --version "$(date +"%Y%m%d%H%M%S")" --input . --output upload --manifest-dest "upload/cosmic_inflation_1.12.2.json"
